b9ef4dd40623d89f01c37ef63ddb46c30e493954
pahlavaubivca <pahlavaubivca@gmail.com>
2024-05-04 12:21:23 +0300

    made led blink

--- blinky.rs
+++ blinky.rs
@@ -8,20 +8,16 @@
 
 #![no_std]
 #![no_main]
-mod lcd;
 
-extern crate embedded_graphics;
+// extern crate embedded_graphics;
 extern crate embedded_hal;
-extern crate embedded_time;
 extern crate panic_halt;
 extern crate rp2040_hal;
-extern crate st7735_lcd;
-extern crate embedded_graphics_core;
 
 // Ensure we halt the program on panic (if we don't mention this crate it won't
 // be linked)
 
-use rp2040_hal::gpio::FunctionSpi;
+use embedded_hal::digital::v2::OutputPin;
 // Alias for our HAL crate
 use rp2040_hal as hal;
 
@@ -29,26 +25,7 @@ use rp2040_hal as hal;
 // register access
 use hal::pac;
 
-// Some traits we need
-use embedded_graphics::image::{Image, ImageRaw, ImageRawLE};
-use embedded_graphics::pixelcolor::Rgb565;
-use embedded_graphics::prelude::*;
-use embedded_hal::digital::v2::OutputPin;
 use rp2040_hal::clocks::Clock;
-use embedded_graphics_core::draw_target::DrawTarget;
-//use embedded_hal::digital::v2::OutputPin;
-use embedded_time::fixed_point::FixedPoint;
-//use embedded_time::rate::Extensions;
-//use rp2040_hal::clocks::Clock;
-//use st7735_lcd;
-use st7735_lcd::Orientation;
-use rp2040_hal::fugit::RateExtU32;
-// use lcd;
-
-/// The linker will place this boot block at the start of our program image. We
-/// need this to help the ROM bootloader get our code up and running.
-/// Note: This boot block is not necessary when using a rp-hal based BSP
-/// as the BSPs already perform this step.
 #[link_section = ".boot2"]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
@@ -101,45 +78,6 @@ fn main() -> ! {
 
     // Configure GPIO25 as an output
     let mut led_pin = pins.gpio25.into_push_pull_output();
-    // These are implicitly used by the spi driver if they are in the correct mode
-    let _spi_sclk = pins.gpio6.into_mode::<hal::gpio::FunctionSpi>();
-    let _spi_mosi = pins.gpio7.into_mode::<hal::gpio::FunctionSpi>();
-    let _spi_miso = pins.gpio4.into_mode::<hal::gpio::FunctionSpi>();
-
-    let sclk = pins.gpio2.into_function::<FunctionSpi>();
-    let mosi = pins.gpio3.into_function::<FunctionSpi>();
-
-    // let spi_device = peripherals.SPI0;
-    let spi_pin_layout = (mosi, sclk);
-
-    let spi = hal::Spi::<_, _, _, 8>::new(pac.SPI0,spi_pin_layout);
-
-    let mut lcd_led = pins.gpio12.into_push_pull_output();
-    // let dc = pins.gpio13.into_push_pull_output();
-    let dc = pins.gpio13.into_readable_output();
-    let rst = pins.gpio14.into_push_pull_output();
-
-    // Exchange the uninitialised SPI driver for an initialised one
-    let spi = spi.init(
-        &mut pac.RESETS,
-        clocks.peripheral_clock.freq(),
-        16_000_000u32.Hz(),
-        &embedded_hal::spi::MODE_0,
-    );
- 
-    let mut disp = st7735_lcd::ST7735::new(spi, dc, rst, true, false, 160, 128);
-
-    disp.init(&mut delay).unwrap();
-    disp.set_orientation(&Orientation::Landscape).unwrap();
-    disp.clear(Rgb565::BLACK).unwrap();
-    disp.set_offset(0, 25);
-
-    let image_raw: ImageRawLE<Rgb565> =
-        ImageRaw::new(include_bytes!("./assets/ferris.raw"), 86);
-
-    let image: Image<_> = Image::new(&image_raw, Point::new(34, 8));
-
-    image.draw(&mut disp).unwrap();
     loop {
         led_pin.set_high().unwrap();
         delay.delay_ms(500);
